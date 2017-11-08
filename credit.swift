ab47fb43b91090737e6077549e96623cfa91560d
Payscout Inc <victor@payscout.com>
2017-11-08 16:55:21 +0200

    Update credit.swift

--- credit.swift
+++ credit.swift
@@ -4,7 +4,7 @@ func sayHello(url: String) {
             "content-type": "application/x-www-form-urlencoded",
         ]
         let apiURL = URL(string: url);
-        let postData = "client_username={yourUsername}&client_password={yourPassword}&client_token=token&processing_type=CREDIT&expiration_month=10&expiration_year=2022&account_number={yourTestCardNumber}&cvv2=123¤cy=USD&initial_amount=99.99".data(using: String.Encoding.utf8);
+        let postData = "client_username={yourUsername}&client_password={yourPassword}&client_token=token&processing_type=CREDIT&expiration_month=10&expiration_year=2022&account_number={yourTestCardNumber}&cvv2=123&currency=USD&initial_amount=99.99".data(using: String.Encoding.utf8);
 
         var request = URLRequest(url: apiURL!)
         request.httpMethod = "POST"
